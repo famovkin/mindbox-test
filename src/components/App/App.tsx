9745c76c0a477abf712ba6cd52d4b41fd7f8105a
Tolya Fam <famovkin@yandex.ru>
2022-06-12 18:07:01 +0300

    create basic structure

---
+++ src/components/App/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__container">
+        <h1 className="app__title">Todos</h1>
+        {/* input */}
+        {/* todos list */}
+        {/* footer */}
+        {/* inside footer filter */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
