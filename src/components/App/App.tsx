8df9c67a6bf5819fbd3125b6844db0fa52c6d1e1
Tolya Fam <famovkin@yandex.ru>
2022-06-14 04:59:09 +0300

    feat: add remote and filter todos feature

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,19 +1,84 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import Input from '../Input/Input';
 import Todo from '../Todo/Todo';
+import Footer from '../Footer/Footer';
 
-import { TodoProps } from '../../types/todo';
+import { TodoState } from '../../types/todo';
 
 import './App.css';
 
-const todos: Array<TodoProps> = [
-  { id: 1, text: 'Сделать что-нибудь важное' },
-  { id: 2, text: 'Купить хлеб' },
-  { id: 3, text: 'Погулять с собакой' },
+const initialTodos = [
+  { id: 1, text: 'Сделать что-нибудь важное', complete: false },
+  { id: 2, text: 'Купить хлеб', complete: false },
+  { id: 3, text: 'Погулять с собакой', complete: false },
+  { id: 4, text: 'Погулять с кошкой', complete: false },
+  { id: 5, text: 'Погулять одному', complete: false },
 ];
 
 function App() {
+  const [filter, setFilter] = useState<string>('all');
+  const [todos, setTodos] = useState<TodoState[]>(initialTodos);
+  const [activeTodosCount, setActiveTodosCount] = useState<number>(todos.length);
+
+  const filterTodo = (selectedFilter: string): void => {
+    setFilter(selectedFilter);
+  };
+
+  useEffect(() => setFilter(filter), [filter]);
+
+  const computeActiveTodo = useCallback(
+    (): number => todos.filter((todo) => todo.complete === false).length,
+    [todos],
+  );
+
+  useEffect(() => setActiveTodosCount(computeActiveTodo()), [todos]);
+
+  const allTodos = filter === 'all'
+    && todos.map((todo) => (
+      <Todo
+        key={todo.id}
+        text={todo.text}
+        id={todo.id}
+        complete={todo.complete}
+        setTodos={setTodos}
+        todos={todos}
+      />
+    ));
+
+  const activeTodos = filter === 'active'
+    && todos
+      .filter((todo) => todo.complete === false)
+      .map((todo) => (
+        <Todo
+          key={todo.id}
+          text={todo.text}
+          id={todo.id}
+          complete={todo.complete}
+          setTodos={setTodos}
+          todos={todos}
+        />
+      ));
+
+  const completedTodos = filter === 'completed'
+    && todos
+      .filter((todo) => todo.complete === true)
+      .map((todo) => (
+        <Todo
+          key={todo.id}
+          text={todo.text}
+          id={todo.id}
+          complete={todo.complete}
+          setTodos={setTodos}
+          todos={todos}
+        />
+      ));
+
+  const removeTodo = () => {
+    const activeTodoItems = todos.filter((todo) => todo.complete === false);
+    setTodos(activeTodoItems);
+  };
+
   return (
     <div className="app">
       <h1 className="app__title">todos</h1>
@@ -23,11 +88,21 @@ function App() {
           placeholder="What needs to be done?"
         />
         <ul className="todos">
-          {todos.length
-            ? todos.map((todo) => (
-              <Todo key={todo.id} text={todo.text} id={todo.id} />))
-            : <h2 className="todos__message">Todo list is empty</h2>}
+          {allTodos && allTodos.length !== 0
+            ? allTodos
+            : filter === 'all' && <h2 className="todos__message">Todo list is empty</h2>}
+          {activeTodos && activeTodos.length !== 0
+            ? activeTodos
+            : filter === 'active' && <h2 className="todos__message">No active tasks</h2>}
+          {completedTodos && completedTodos.length !== 0
+            ? completedTodos
+            : filter === 'completed' && <h2 className="todos__message">No completed tasks</h2>}
         </ul>
+        <Footer
+          activeTodosCount={activeTodosCount}
+          filterHandler={filterTodo}
+          removeHandler={removeTodo}
+        />
       </div>
     </div>
   );
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Button from '../Button/Button';
 
@@ -8,17 +8,21 @@ import { FooterProps } from '../../types/footer';
 import './Footer.css';
 
 function Footer({ activeTodosCount, filterHandler, removeHandler }: FooterProps) {
+  const [activeFilter, setActiveFilter] = useState<number>(0);
+
   const changeFilter = (selectedFilter: string): void => {
     filterHandler(selectedFilter);
+    const selectedFilterIndex = filters.findIndex((item) => item.value === selectedFilter);
+    setActiveFilter(selectedFilterIndex);
   };
 
   return (
     <footer className="footer">
       <p className="footer__remains">{`${activeTodosCount} items left`}</p>
       <ul className="footer__filters">
-        {filters.length && filters.map((filter) => (
+        {filters.length && filters.map((filter, index) => (
           <li
-            className="filter"
+            className={`filter ${activeFilter === index ? 'filter_type_active' : ''}`}
             key={filter.id}
             onClick={() => changeFilter(filter.value)}
             role="presentation"
