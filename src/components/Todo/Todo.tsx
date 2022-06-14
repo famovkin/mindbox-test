1703dc25542c382bd6a968d630dfa08aa2946193
Tolya Fam <famovkin@yandex.ru>
2022-06-14 04:44:26 +0300

    feat: add check todo feature

--- src/components/Todo/Todo.tsx
+++ src/components/Todo/Todo.tsx
@@ -4,19 +4,36 @@ import { TodoProps } from '../../types/todo';
 
 import './Todo.css';
 
-function Todo({ text, id }: TodoProps) {
+function Todo({
+  text,
+  id,
+  complete,
+  todos,
+  setTodos,
+}: TodoProps) {
+  const checkboxHandler = (todoId: number) => {
+    const selectedTodoIndex = todos?.findIndex((todo) => todo.id === todoId);
+    const selectedTodo = todos[selectedTodoIndex];
+    const updatedTodo = { ...selectedTodo, complete: !selectedTodo.complete };
+    const updatedTodos = [...todos];
+    updatedTodos[selectedTodoIndex] = updatedTodo;
+    setTodos(updatedTodos);
+  };
+
   return (
     <li className="todo">
       <label className="todo__label" htmlFor={`${text + id}`}>
         <input
-          className="todo__radio"
+          className="todo__checkbox"
           type="checkbox"
           id={`${text + id}`}
+          checked={complete}
+          onChange={() => checkboxHandler(id)}
         />
         <div className="todo__pseudo-item">
           <span className="todo__checkmark" />
         </div>
-        <p className="todo__text">{text}</p>
+        <p className={`todo__text ${complete ? 'todo__text_type_checked' : ''}`}>{text}</p>
       </label>
     </li>
   );
--- src/types/todo.ts
+++ src/types/todo.ts
@@ -1,4 +1,12 @@
-export interface TodoProps {
+import { Dispatch, SetStateAction } from 'react';
+
+export interface TodoState {
   text: string;
   id: number;
+  complete: boolean;
+}
+
+export interface TodoProps extends TodoState {
+  todos: Array<TodoState>;
+  setTodos: Dispatch<SetStateAction<TodoState[]>>;
 }
