8f1e71eb2f3e4fd464169e1b2e342f468fb91a88
Tolya Fam <famovkin@yandex.ru>
2022-06-16 00:40:37 +0300

    feat: add test for App

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  test('Renders without crashing', () => {
+    render(<App />);
+
+    expect(screen.getByText('todos')).toBeInTheDocument();
+  });
 });
